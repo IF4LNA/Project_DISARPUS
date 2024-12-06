d7c55428b0ccf3d35aad3cc13f83a0b4f171fd88
Thoha Ramadhan <thoharamadhann20@gmail.com>
2024-12-06 12:33:44 +0700

    update

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -93,6 +93,8 @@
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
+            {{-- navbar-list --}}
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                     <li class="nav-item">
